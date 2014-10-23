8b423cbb951ff5d2dfe672e245e38b1a8f7ea8a6
Jose R. Ramirez A <jose.ramirez@driverontap.com>
2014-10-22 22:40:47 -0400

    Hangman Game implementation

--- HangmanWord.swift
+++ HangmanWord.swift
@@ -8,18 +8,37 @@
 
 import Foundation
 
+// Class that implements hangman game
 class HangmanWord {
     
     var mWord: String
+    var mGuess: String
+    var mLife: Int
     
     init(word: String){
+        
         mWord = word
+        mGuess = ""
+        mLife = 0
+        
+        if IsWordGood()
+        {
+            mLife = 10
+            mGuess = ""
+        
+            for character in mWord
+            {
+                mGuess += "_ "
+            }
+        }
     }
     
-    func IsWordGood() -> BooleanType {
+    func IsWordGood() -> Bool {
         
-        if mWord == "Hangman"
+        if mWord == "Secret"
         {
+            // check that is it only letters
+            // no spaces, numbers or punctuation
             return true
         }
         else
@@ -27,4 +46,24 @@ class HangmanWord {
             return false
         }
     }
+    
+    func GuessCharacter(char: Character) -> Bool
+    {
+        if mWord == "Hangman" // check if character is in string
+        {
+            // put char in correct index in mGuess
+            return true
+
+        }
+        
+        mLife -= 1
+        return false
+        
+    }
+    
+    func GetLife() -> Int
+    {
+        return mLife
+    }
+    
 }
